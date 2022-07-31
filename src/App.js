63d5ca76c8531cb0f4da0133c3a2da99e93d04f5
yeonvv <yeonvv8@gmail.com>
2022-07-31 23:11:17 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  const onClick = () => {
+    setCounter(counter + 1);
+  };
+  useEffect(() => {
+    console.log("I run one time");
+  }, []);
+  useEffect(() => {
+    console.log("Call the API");
+  }, [counter]);
+  useEffect(() => {
+    console.log("SEARCH FOR: ", keyword);
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click</button>
     </div>
   );
 }
